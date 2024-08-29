dba60a4c9d6df654fc5b1054a9b2039185f665ec
Wyne <izmodenov1997@gmail.com>
2024-08-29 21:59:45 +0500

    Add static api getter

--- paper/src/main/java/org/nevermined/worldevents/WorldEventsApi.java
+++ paper/src/main/java/org/nevermined/worldevents/WorldEventsApi.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.nevermined.worldevents.api.WEApi;
 import org.nevermined.worldevents.api.config.GlobalConfigApi;
 import org.nevermined.worldevents.api.expansions.ExpansionData;
 import org.nevermined.worldevents.api.core.WorldEventAction;
@@ -27,6 +28,7 @@ public class WorldEventsApi implements org.nevermined.worldevents.api.WorldEvent
         this.plugin = plugin;
         this.expansionRegistry = expansionRegistry;
         plugin.getServer().getServicesManager().register(org.nevermined.worldevents.api.WorldEventsApi.class, this, plugin, ServicePriority.Normal);
+        WEApi.setInstance(this);
     }
 
     @Override
